e0588562aa682964c755de4654254a9959777d1f
RichardTeoh <richardhung88@hotmail.com>
2025-07-06 15:32:19 +0800

    Update header-paths.js

--- scripts/header-paths.js
+++ scripts/header-paths.js
@@ -1,4 +1,5 @@
 function adjustHeaderPaths(level) {
+  /*
   let prefix = level == 0 ? "" : "../";
   $("#header img").attr("src", prefix + "assets/LogoSillentLibrary.jpg");
   $("#header a.nav-link[href$='index.html']").attr("href", prefix + "index.html");
@@ -6,4 +7,5 @@ function adjustHeaderPaths(level) {
   $("#header a.nav-link[href$='events.html']").attr("href", prefix + "pages/events.html");
   $("#header a.nav-link[href$='contact.html']").attr("href", prefix + "pages/contact.html");
   $("#header a[href$='index.html'] img").parent().attr("href", prefix + "index.html");
+  */
 }
