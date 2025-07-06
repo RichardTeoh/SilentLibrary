5efd35f5f0c7962858199a564657d19fedad3ffa
RichardTeoh <richardhung88@hotmail.com>
2025-07-06 15:38:48 +0800

    Update header-paths.js

--- scripts/header-paths.js
+++ scripts/header-paths.js
@@ -1,11 +1,13 @@
 function adjustHeaderPaths(level) {
-  /*
-  let prefix = level == 0 ? "" : "../";
-  $("#header img").attr("src", prefix + "assets/LogoSillentLibrary.jpg");
-  $("#header a.nav-link[href$='index.html']").attr("href", prefix + "index.html");
-  $("#header a.nav-link[href$='books.html']").attr("href", prefix + "pages/books.html");
-  $("#header a.nav-link[href$='events.html']").attr("href", prefix + "pages/events.html");
-  $("#header a.nav-link[href$='contact.html']").attr("href", prefix + "pages/contact.html");
-  $("#header a[href$='index.html'] img").parent().attr("href", prefix + "index.html");
-  */
+  let prefix = level === 0 ? "" : "../";
+
+  // Update image and logo link
+  $("#logo-img").attr("src", prefix + "assets/LogoSillentLibrary.jpg");
+  $("#logo-link").attr("href", prefix + "index.html");
+
+  // Update nav links
+  $("#nav-home").attr("href", prefix + "index.html");
+  $("#nav-books").attr("href", prefix + "pages/books.html");
+  $("#nav-events").attr("href", prefix + "pages/events.html");
+  $("#nav-contact").attr("href", prefix + "pages/contact.html");
 }
