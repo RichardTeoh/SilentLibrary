ee7b1628f0dbe3b3f56473b0c652a433c247fb8f
RichardTeoh <richardhung88@hotmail.com>
2025-07-07 12:46:51 +0800

    Re-Upload all files

---
+++ scripts/header-paths.js
@@ -0,0 +1,14 @@
+
+function adjustHeaderPaths(level) {
+  let prefix = level === 0 ? "" : "../";
+
+  // Update image and logo link
+  $("#logo-img").attr("src", prefix + "assets/SL-Logo.png");
+  $("#logo-link").attr("href", prefix + "index.html");
+
+  // Update nav links
+  $("#nav-home").attr("href", prefix + "index.html");
+  $("#nav-books").attr("href", prefix + "pages/books.html");
+  $("#nav-events").attr("href", prefix + "pages/events.html");
+  $("#nav-contact").attr("href", prefix + "pages/contact.html");
+}
