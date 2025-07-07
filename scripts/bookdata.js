d2259d293f269559e64274402c608b11c91fbf5d
RichardTeoh <richardhung88@hotmail.com>
2025-07-07 17:40:17 +0800

    upload bookdata

---
+++ scripts/bookdata.js
@@ -0,0 +1,108 @@
+// scripts/bookdata.js
+const bookData = [
+    {
+        referenceNo: "B00001",
+        title: "Atmosphere",
+        author: "Taylor Jenkins Reid",
+        publisher: "Ballantine Books",
+        publishedDate: "1970-01-01"
+    },
+    {
+        referenceNo: "B00002",
+        title: "Sunrise on the Reaping",
+        author: "Suzanne Collins",
+        publisher: "Scholastic",
+        publishedDate: "2025-03-18"
+    },
+    {
+        referenceNo: "B00003",
+        title: "The Let Them Theory",
+        author: "Mel Robbins & Sawyer Robbins",
+        publisher: "Hay House Inc.",
+        publishedDate: "2024-12-24"
+    },
+    {
+        referenceNo: "B00004",
+        title: "Fourth Wing",
+        author: "Rebecca Yarros",
+        publisher: "Red Tower Books",
+        publishedDate: "1970-01-01"
+    },
+    {
+        referenceNo: "B00005",
+        title: "Never Flinch",
+        author: "Stephen King",
+        publisher: "Scribner",
+        publishedDate: "2025-05-27"
+    },
+    {
+        referenceNo: "B00006",
+        title: "A Council of Dolls",
+        author: "Mona Susan Power",
+        publisher: "Mariner Books (HarperCollins)",
+        publishedDate: "2023-08-08"
+    },
+    {
+        referenceNo: "B00007",
+        title: "The Spirit Bares Its Teeth",
+        author: "Andrew Joseph White",
+        publisher: "—",
+        publishedDate: "2023-09-05"
+    },
+    {
+        referenceNo: "B00008",
+        title: "Yellowface",
+        author: "R.F. Kuang",
+        publisher: "—",
+        publishedDate: "1970-01-01"
+    },
+    {
+        referenceNo: "B00009",
+        title: "The Heaven & Earth Grocery Store",
+        author: "James McBride",
+        publisher: "—",
+        publishedDate: "1970-01-01"
+    },
+    {
+        referenceNo: "B00010",
+        title: "Cobalt Red",
+        author: "Siddharth Kara",
+        publisher: "—",
+        publishedDate: "1970-01-01"
+    },
+    {
+        referenceNo: "B00011",
+        title: "My Sister’s Keeper",
+        author: "Jodi Picoult",
+        publisher: "—",
+        publishedDate: "1970-01-01"
+    },
+    {
+        referenceNo: "B00012",
+        title: "Cloud Atlas",
+        author: "David Mitchell",
+        publisher: "—",
+        publishedDate: "1970-01-01"
+    },
+    {
+        referenceNo: "B00013",
+        title: "P.S. I Love You",
+        author: "Cecelia Ahern",
+        publisher: "—",
+        publishedDate: "1970-01-01"
+    },
+    {
+        referenceNo: "B00014",
+        title: "Eragon",
+        author: "Christopher Paolini",
+        publisher: "Knopf",
+        publishedDate: "1970-01-01"
+    },
+    {
+        referenceNo: "B00015",
+        title: "The Da Vinci Code",
+        author: "Dan Brown",
+        publisher: "Doubleday",
+        publishedDate: "1970-01-01"
+    }
+];
